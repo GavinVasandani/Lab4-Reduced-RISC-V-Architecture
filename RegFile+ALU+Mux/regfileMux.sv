0df5f5ba9dd6a66a18091542f09e26fb30eac6a4
GavinVasandani <89224315+GavinVasandani@users.noreply.github.com>
2022-11-17 05:09:53 -0900

    Finished ALU and mux

--- RegFile+ALU+Mux/regfileMux.sv
+++ RegFile+ALU+Mux/regfileMux.sv
@@ -1,1 +1,18 @@
-//Creating mux that's after regFile
+//Creating ALU Mux
+//Now create top level design which connects regfile, mux, alu with wires
+
+module ALUMux # (
+    parameter Data_Width = 32,
+
+) (
+    //Interface signals
+    input logic [Data_Width-1:0] regOp2; //Output if mux select is 0
+    input logic [Data_Width-1:0] ImmOp; //Output if mux select is 1
+    input ALUSrc; //mux select
+    output logic [Data_Width-1:0] ALUOp2; //2nd output of ALU
+);
+
+ALUOp2 = (ALUsrc) ? regOp2:ImmOp;
+
+endmodule
+
