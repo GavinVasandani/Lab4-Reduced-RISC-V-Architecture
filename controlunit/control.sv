b3f288b3fa7cb9ab015b3405170140dd930273e5
xuhpan2027 <46816781+xuhpan2028@users.noreply.github.com>
2022-11-19 23:07:11 +0000

    Add files via upload

---
+++ controlunit/control.sv
@@ -0,0 +1,34 @@
+module control(
+    input logic         EQ,
+    input logic[31:0]   instr,
+
+    output logic RegWrite,
+    output logic[2:0] ALUctrl,
+    output logic ALUsrc,
+    output logic ImmSrc,
+    output logic PCsrc
+);
+
+
+always_comb begin
+
+    case(instr[6:0])
+        7'b0010011: begin ALUctrl = instr[14:12];
+                          RegWrite = 1;
+                          ALUsrc = 1;
+                          ImmSrc = 1;
+                          PCsrc = 0;
+        end 
+
+    
+        7'b1100011: begin RegWrite = 0;
+                          ALUsrc = 0;
+                          ImmSrc = 0;
+                          PCsrc = 1;
+        end
+
+
+    
+end
+    
+endmodule
