05c7e04e8a1944d51426794005617bcb49ee3b88
Arnav Kohli <92183528+arnavxkohli@users.noreply.github.com>
2022-11-23 21:07:15 +0000

    Update ext.sv

--- test/ext.sv
+++ test/ext.sv
@@ -1,27 +1,16 @@
 module ext(
-    // input clk,
-    // input logic [31:0] inst,
     input logic [11:0] imm_imm, // imm value when the instruction format is immediate
-    input logic [11:0] imm_branch, // imm value when the instruction format is branch
+    input logic [12:0] imm_branch, // imm value when the instruction format is branch
     input logic ImmSrc,
     output logic [31:0] ImmOp
 );
 
-    logic [11:0] imm;
-
     always_comb begin
         if (ImmSrc) 
-        imm = imm_imm;
+            ImmOp = {{20{imm_imm[11]}}, imm_imm};
 
         else 
-        imm = imm_branch;
-
-
-
-        ImmOp = {{20{imm[11]}}, imm[11:0]};
+            ImmOp = {{19{imm_branch[12]}}, imm_branch};
     end
 
 endmodule
-    
-
-
