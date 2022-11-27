a4f24abc7796910a24a1a4ac4d414c0a5796574f
xuhpan2028 <2456335262@qq.com>
2022-11-27 15:43:12 +0000

    modified lw instruction in controlunit folder

--- controlunit/control.sv
+++ controlunit/control.sv
@@ -2,7 +2,7 @@ module control(
     input logic         EQ,
     input logic [6:0]   instr_opcode,
 
-    // output logic RegWrite,
+    output logic RegWrite,
     output logic[2:0] ALUctrl,
     output logic ALUsrc,
     output logic ImmSrc,
@@ -12,20 +12,36 @@ module control(
 
 always_comb begin
 
+    ALUctrl = funct3;
+
     case(instr_opcode)
         7'b0010011: begin ALUctrl = 3'b000;
-                        //   RegWrite = 1;
+                          RegWrite = 1;     //addi
                           ALUsrc = 1;
                           ImmSrc = 1;
                           PCsrc = 0;
         end 
 
     
-        7'b1100011: begin ALUctrl = 3'b111;
-            // RegWrite = 0;
+        7'b1100011: begin ALUctrl = 3'b111;     //bne
                           ALUsrc = 0;
                           ImmSrc = 0;
-                          PCsrc = 1;
+                          PCsrc = ~EQ;
+        end
+
+
+        7'b0000011: begin ALUctrl = 3'b000;
+                          RegWrite = 0;     //lw
+                          ALUsrc = 1;
+                          ImmSrc = 1;
+                          PCsrc = 0;
+        end
+
+
+        default: begin
+            ALUsrc = 0;
+            ImmSrc = 0;
+            PCsrc = 0;
         end
 
     endcase
